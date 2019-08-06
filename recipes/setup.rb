dca5e5794c75223106f63a8e5e5d046f823a0fea
andreinavroschi <andrei.navroschi@gmail.com>
2019-08-06 15:43:20 +0200

    Adding node attributes to motd file

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -14,9 +14,18 @@ end
 
 package 'ntp'
 
+# node['ipaddress']
+# node['memory']['total']
+
+
 file '/etc/motd' do
   action :create
-  content 'This server is property of Andrei Navroschi'
+  content "This server is property of Andrei Navroschi
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+"
   owner 'root'
   group 'root'
   mode '644'
